4d58e43029ee4a73b0b33f798da441045f6b6a4a
Josh Arrants <duhhobo@gmail.com>
2020-01-23 21:36:43 -0500

    Initial template created

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def homepage(request):
+    context = {}
+    return render(request, 'main/home.html', context)
